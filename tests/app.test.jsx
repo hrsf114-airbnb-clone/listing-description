7ed35abab7e2229b14fa93a7ade4bb038fb0acea
Lamanova <44875532+Lamanova@users.noreply.github.com>
2019-04-04 08:25:31 -0700

    Revert "Generate seed data and set up API"

--- tests/app.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../client/src/components/App';
-
-it('should have text', () => {
-  const wrap = shallow(<App />);
-  expect(wrap.text()).toEqual('hello world');
-});
