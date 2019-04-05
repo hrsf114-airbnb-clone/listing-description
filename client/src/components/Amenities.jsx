bc8f724b5ca1bfaa84564921bc88148872252b3d
Victoria <lamanovavi21@gmail.com>
2019-04-04 18:10:16 -0700

    Add amenities modal

--- client/src/components/Amenities.jsx
+++ client/src/components/Amenities.jsx
@@ -4,9 +4,6 @@ import { tsImportEqualsDeclaration } from '@babel/types';
 class Amenities extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      showModal: false,      
-    }
     this.amenitiesIcon = {
       wifi: 'fa-wifi',
       dryer: 'fa-tshirt',
@@ -15,63 +12,93 @@ class Amenities extends React.Component {
       parking: 'fa-parking',
       washer: 'fa-tint'
     }
+
+    this.notes = {
+      wifi: 'Continuous access in the listing',
+      dryer: 'In the building, free or for a fee',
+      'laptop friendly workspace': 'A table or desk with space for a laptop and a chair that’s comfortable to work in',
+      washer: 'In the building, free or for a fee',
+      essentials: 'Towels, bed sheets, soap, and toilet paper',
+      heating: 'Central heating or a heater in the listing',
+      kitchen: 'Space where guests can cook their own meals',      
+    }
   }
 
   getTopAmenities(currentRoom) {
     let basicAmenities = currentRoom.Amenities.basic
-    return  Object.keys(basicAmenities).filter((key) => basicAmenities[key]).slice(0, 4);  
+    return Object.keys(basicAmenities).filter((key) => basicAmenities[key]).slice(0, 4);
   }
-  
-  AmenitiesModal() {
-    const { currentRoom } = this.props;
-    return (
-      <div className="modal fade" id="modal" tabIndex="-1" role="dialog" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              {/* {
-                for (let service in currentRoom.Amenities) {
 
-              }
-               } */}
+  getDesc(subCategory) {
+    if (this.notes[subCategory]) {
+      return (<div className="thin">{this.notes[subCategory]}</div>)
+    } else {
+      return (<div></div>)
+    }
+
+  }
+
+  AmenitiesModal(amenities) {
+    const content = Object.keys(amenities).map((category) => {      
+      return(  
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-12 capitalize category"><b>{category}</b></div>          
+              {Object.keys(amenities[category]).map((subCategory) => {                   
+                  return (
+                    <div className="col-md-12 capitalize">
+                      {subCategory}                      
+                      {this.getDesc(subCategory)}                                            
+                      <hr/>
+                    </div>                  
+                  )
+              })}
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+          </div>      
+      )            
+    });
+    return (
+      <div className="modal fade" id="modal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-body">
+              <div className="row">
+                <div className="col-md-12">
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close" style={{ float: 'left' }}>
+                    <span aria-hidden="true">&times;</span>                                
+                  </button>
+                </div>
+                <div className="col-md-12 category">
+                  <h2>Amenities</h2>
+                </div>
+                {content}
+              </div>
             </div>
           </div>
         </div>
       </div>
     )
-
   }
 
   render() {
     const { currentRoom } = this.props;
-    const { showModal } = this.state;
     const top = this.getTopAmenities(currentRoom);
     return (
-      <div className="container">
-        <div className="row">
-          {
-            top.map((el, index) => {                            
-              return (
-                <div className="col-md-6" key={index}>
-                  <i className={`fas ${this.amenitiesIcon[el]} icon-main`}></i>
-                  {el}
-                </div>
-              )
-            })
-          }
+      <div className="row">
+        {
+          top.map((el, index) => {
+            return (
+              <div className="col-md-6" key={index}>
+                <i className={`fas ${this.amenitiesIcon[el]} icon-main`}></i>
+                {el}
+              </div>
+            )
+          })
+        }
+        <div className="col-md-12">
+          <a href="#" role="button" data-toggle="modal" data-target="#modal">See all amenities</a>
+          {this.AmenitiesModal(currentRoom.Amenities)}
         </div>
-        <a data-toggle="modal" data-target="#modal">See all amenities</a>
-        {this.AmenitiesModal()}
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,23 @@
 const path = require('path');
-const webpack = require('webpack')
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  //context: path.resolve(__dirname + '/client'),
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: path.resolve(__dirname, 'client/src/index.jsx'),
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'env']
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
-      },
+      }      
     ],
   },
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: path.resolve(__dirname, 'client/dist'),
   },
 };
-
-
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist')
-//   }
-// };
-
-// module.exports = {
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: DIST_DIR
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?/,
-//         include: SRC_DIR,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['react', 'es2015']
-//         }
-//       }
-//     ]
-//   }
-// };
